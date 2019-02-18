acf51ceb179b9730c82ddcf7bf6825509f915605
Karell Nixon <nixonkarell@gmail.com>
2019-02-18 11:52:11 -0500

    first step for collision

--- darkpg.py
+++ darkpg.py
@@ -221,6 +221,8 @@ def levelOne():
         def moveDown(self):
             self.y = self.y + self.speed
 
+
+
     class Maze:
         def __init__(self):
            self.M = 23
@@ -407,6 +409,40 @@ def levelFour():
     running = True
     event = pygame.event.wait()
     print("Level four has started!")
+
+
+        def __init__(self):
+            self.rect = pygame.Rect(32, 32, 16, 16)
+
+        def move(self, dx, dy):
+            
+            # Move each axis separately. Note that this checks for collisions both times.
+            if dx != 0:
+                self.move_single_axis(dx, 0)
+            if dy != 0:
+                self.move_single_axis(0, dy)
+        
+        def move_single_axis(self, dx, dy):
+            
+            # Move the rect
+            self.rect.x += dx
+            self.rect.y += dy
+
+            # If you collide with a wall, move out based on velocity
+            for wall in walls:
+                if self.rect.colliderect(wall.rect):
+                    if dx > 0: # Moving right; Hit the left side of the wall
+                        self.rect.right = wall.rect.left
+                    if dx < 0: # Moving left; Hit the right side of the wall
+                        self.rect.left = wall.rect.right
+                    if dy > 0: # Moving down; Hit the top side of the wall
+                        self.rect.bottom = wall.rect.top
+                    if dy < 0: # Moving up; Hit the bottom side of the wall
+                        self.rect.top = wall.rect.bottom
+
+
+
+
 
 
 
